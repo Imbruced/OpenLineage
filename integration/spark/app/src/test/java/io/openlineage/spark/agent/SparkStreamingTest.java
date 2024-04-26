113aa050b0af55eb2fc23eec7c21376c604da799
Pawel Kocinski <pawel93kocinski@gmail.com>
2024-04-26 13:40:49 +0200

    Add kafka input source when writing in foreach batch mode.
    Signed-off-by: pawelkocinski <pawel93kocinski@gmail.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/SparkStreamingTest.java
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/SparkStreamingTest.java
@@ -68,6 +68,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 import org.testcontainers.utility.DockerImageName;
 
@@ -100,79 +101,85 @@ class SparkStreamingTest {
     }
   }
 
+  @Getter
+  static class KafkaTestContainer {
+    private final KafkaContainer kafka;
+    private final String sourceTopic;
+    private final String targetTopic;
+    private final String bootstrapServers;
+
+    public KafkaTestContainer(
+        KafkaContainer kafka, String sourceTopic, String targetTopic, String bootstrapServers) {
+      this.kafka = kafka;
+      this.sourceTopic = sourceTopic;
+      this.targetTopic = targetTopic;
+      this.bootstrapServers = bootstrapServers;
+    }
+
+    public void stop() {
+      kafka.stop();
+    }
+
+    public void close() {
+      kafka.close();
+    }
+
+    public boolean isRunning() {
+      return kafka.isRunning();
+    }
+  }
+
+  @Getter
+  static class PostgreSQLTestContainer {
+    private final PostgreSQLContainer<?> postgres;
+
+    public PostgreSQLTestContainer(PostgreSQLContainer<?> postgres) {
+      this.postgres = postgres;
+    }
+
+    public void stop() {
+      postgres.stop();
+    }
+
+    public String getNamespace() {
+      return postgres.getHost() + ":" + postgres.getMappedPort(5432).toString();
+    }
+  }
+
   @Nested
   class Kafka {
-
     @Test
     @EnabledIfSystemProperty(named = SPARK_VERSION, matches = SPARK_3_OR_ABOVE)
     void testKafkaSourceToKafkaSink()
         throws TimeoutException, StreamingQueryException, IOException {
-      KafkaContainer kafka =
-          new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1"));
-      kafka.start();
+      KafkaTestContainer kafkaContainer = setupKafkaContainer();
 
-      String bootstrapServers = kafka.getBootstrapServers();
+      String bootstrapServers = kafkaContainer.getBootstrapServers();
 
-      createTopics(bootstrapServers, Arrays.asList("source-topic", "target-topic"));
-      populateTopic(bootstrapServers, "source-topic");
-
-      String userDirProperty = System.getProperty("user.dir");
-      Path userDirPath = Paths.get(userDirProperty);
       UUID testUuid = UUID.randomUUID();
       log.info("TestUuid is {}", testUuid);
 
-      Path derbySystemHome =
-          userDirPath.resolve("tmp").resolve("derby").resolve(testUuid.toString());
-      Path sparkSqlWarehouse =
-          userDirPath.resolve("tmp").resolve("spark-sql-warehouse").resolve(testUuid.toString());
+      OpenLineageEndpointHandler handler = new OpenLineageEndpointHandler();
+      HttpServer server = createHttpServer(handler);
+
+      SparkSession spark = createSparkSession(server.getAddress().getPort());
+      spark.sparkContext().setLogLevel("ERROR");
+
+      String userDirProperty = System.getProperty("user.dir");
+      Path userDirPath = Paths.get(userDirProperty);
+
       Path checkpointsDir =
           userDirPath.resolve("tmp").resolve("checkpoints").resolve(testUuid.toString());
 
-      int randomPort = new Random().nextInt(1000) + 10000;
-
-      OpenLineageEndpointHandler handler = new OpenLineageEndpointHandler();
-      HttpServer server = HttpServer.create(new InetSocketAddress(randomPort), 0);
-      server.createContext("/api/v1/lineage", handler);
-      server.setExecutor(null);
-      server.start();
-
-      SparkSession spark =
-          SparkSession.builder()
-              .appName("kafka-source-kafka-sink")
-              .master("local[*]")
-              .config("spark.extraListeners", OpenLineageSparkListener.class.getCanonicalName())
-              .config("spark.driver.host", "localhost")
-              .config("spark.driver.extraJavaOptions", "-Dderby.system.home=" + derbySystemHome)
-              .config("spark.sql.warehouse.dir", sparkSqlWarehouse.toString())
-              .config("spark.ui.enabled", false)
-              .config("spark.openlineage.transport.type", "http")
-              .config("spark.openlineage.transport.url", "http://localhost:" + randomPort)
-              .config("spark.openlineage.facets.disabled", "[spark_unknown;]")
-              .getOrCreate();
-
       Dataset<Row> sourceStream =
-          spark
-              .readStream()
-              .format("kafka")
-              .option("subscribe", "source-topic")
-              .option("kafka.bootstrap.servers", bootstrapServers)
-              .option("startingOffsets", "earliest")
-              .load();
+          readKafkaTopic(spark, kafkaContainer.sourceTopic, bootstrapServers)
+              .transform(this::processKafkaTopic);
 
-      StructType schema = StructType.fromDDL("id STRING, epoch LONG");
       StreamingQuery streamingQuery =
           sourceStream
-              .selectExpr("CAST(value AS STRING) AS value")
-              .select(from_json(col("value"), schema).as("event"))
-              .select(col("event.id").as("id"), col("event.epoch").as("epoch"))
-              .select(col("id"), from_unixtime(col("epoch")).as("timestamp"))
-              .select(functions.struct(col("id"), col("timestamp")).as("converted_event"))
-              .select(
-                  expr("CAST('1' AS BINARY) AS key"),
-                  functions.to_json(col("converted_event")).as("value"))
               .writeStream()
               .format("kafka")
-              .option("topic", "target-topic")
+              .option("topic", kafkaContainer.targetTopic)
               .option("kafka.bootstrap.servers", bootstrapServers)
               .option("checkpointLocation", checkpointsDir.toString())
               .trigger(Trigger.ProcessingTime(Duration.ofSeconds(4).toMillis()))
@@ -182,11 +189,11 @@ class SparkStreamingTest {
 
       spark.stop();
 
-      kafka.stop();
+      kafkaContainer.stop();
 
-      kafka.close();
+      kafkaContainer.close();
 
-      Awaitility.await().atMost(Duration.ofSeconds(60)).until(() -> !kafka.isRunning());
+      Awaitility.await().atMost(Duration.ofSeconds(60)).until(() -> !kafkaContainer.isRunning());
       Awaitility.await()
           .atMost(Duration.ofSeconds(60))
           .until(() -> spark.sparkContext().isStopped());
@@ -225,7 +232,7 @@ class SparkStreamingTest {
       nonEmptyInputEvents.forEach(
           event -> {
             assertEquals(1, event.getInputs().size());
-            assertEquals("source-topic", event.getInputs().get(0).getName());
+            assertEquals(kafkaContainer.sourceTopic, event.getInputs().get(0).getName());
             assertEquals("kafka://" + bootstrapServers, event.getInputs().get(0).getNamespace());
 
             OpenLineage.SchemaDatasetFacet inputSchema =
@@ -236,7 +243,7 @@ class SparkStreamingTest {
             assertEquals(expectedInputSchema, inputSchemaFields);
 
             assertEquals(1, event.getOutputs().size());
-            assertEquals("target-topic", event.getOutputs().get(0).getName());
+            assertEquals(kafkaContainer.targetTopic, event.getOutputs().get(0).getName());
             assertEquals("kafka://" + bootstrapServers, event.getOutputs().get(0).getNamespace());
 
             OpenLineage.SchemaDatasetFacet outputSchema =
@@ -246,6 +253,242 @@ class SparkStreamingTest {
 
             assertEquals(expectedOutputSchema, outputSchemaFields);
           });
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = SPARK_VERSION, matches = SPARK_3_OR_ABOVE)
+    void testKafkaSourceToBatchSink()
+        throws IOException, TimeoutException, StreamingQueryException {
+      KafkaTestContainer kafkaContainer = setupKafkaContainer();
+
+      String bootstrapServers = kafkaContainer.getBootstrapServers();
+
+      UUID testUuid = UUID.randomUUID();
+      log.info("TestUuid is {}", testUuid);
+
+      OpenLineageEndpointHandler handler = new OpenLineageEndpointHandler();
+      HttpServer server = createHttpServer(handler);
+
+      SparkSession spark = createSparkSession(server.getAddress().getPort());
+      spark.sparkContext().setLogLevel("ERROR");
+
+      Dataset<Row> sourceStream =
+          readKafkaTopic(spark, kafkaContainer.sourceTopic, bootstrapServers)
+              .transform(this::processKafkaTopic);
+
+      StreamingQuery streamingQuery =
+          sourceStream
+              .writeStream()
+              .foreachBatch(
+                  (batch, batchId) -> {
+                    batch.write().format("csv").mode("append").save("/tmp/batch_sink");
+                  })
+              .start();
+
+      streamingQuery.awaitTermination(Duration.ofSeconds(20).toMillis());
+      List<RunEvent> events =
+          handler.eventsContainer.stream()
+              .map(OpenLineageClientUtils::runEventFromJson)
+              .collect(Collectors.toList());
+
+      assertEquals(7, events.size());
+
+      List<RunEvent> kafkaInputEvents =
+          events.stream().filter(x -> !x.getInputs().isEmpty()).collect(Collectors.toList());
+
+      assertEquals(2, kafkaInputEvents.size());
+
+      kafkaInputEvents.forEach(
+          event -> {
+            assertEquals(1, event.getInputs().size());
+            assertEquals(kafkaContainer.sourceTopic, event.getInputs().get(0).getName());
+            assertEquals("kafka://" + bootstrapServers, event.getInputs().get(0).getNamespace());
+          });
+
+      List<RunEvent> outputEvents =
+          events.stream().filter(x -> !x.getOutputs().isEmpty()).collect(Collectors.toList());
+
+      assertEquals(4, outputEvents.size());
+
+      outputEvents.forEach(
+          event -> {
+            assertEquals(1, event.getOutputs().size());
+            assertEquals("/tmp/batch_sink", event.getOutputs().get(0).getName());
+            assertEquals("file", event.getOutputs().get(0).getNamespace());
+          });
+
+      kafkaContainer.stop();
+      spark.stop();
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = SPARK_VERSION, matches = SPARK_3_OR_ABOVE)
+    void testKafkaSourceToJdbcBatchSink()
+        throws IOException, TimeoutException, StreamingQueryException {
+      KafkaTestContainer kafkaContainer = setupKafkaContainer();
+      PostgreSQLTestContainer postgresContainer = startPostgresContainer();
+
+      postgresContainer.postgres.getJdbcUrl();
+
+      String bootstrapServers = kafkaContainer.getBootstrapServers();
+
+      UUID testUuid = UUID.randomUUID();
+      log.info("TestUuid is {}", testUuid);
+
+      OpenLineageEndpointHandler handler = new OpenLineageEndpointHandler();
+      HttpServer server = createHttpServer(handler);
+
+      SparkSession spark = createSparkSession(server.getAddress().getPort());
+      spark.sparkContext().setLogLevel("ERROR");
+
+      Dataset<Row> sourceStream =
+          readKafkaTopic(spark, kafkaContainer.sourceTopic, bootstrapServers)
+              .transform(this::processKafkaTopic);
+
+      StreamingQuery streamingQuery =
+          sourceStream
+              .writeStream()
+              .foreachBatch(
+                  (batch, batchId) -> {
+                    batch
+                        .write()
+                        .format("jdbc")
+                        .option("url", postgresContainer.getPostgres().getJdbcUrl())
+                        .option("driver", "org.postgresql.Driver")
+                        .option("dbtable", "public.test")
+                        .option("user", postgresContainer.getPostgres().getUsername())
+                        .option("password", postgresContainer.getPostgres().getPassword())
+                        .mode("append")
+                        .save();
+                  })
+              .start();
+
+      streamingQuery.awaitTermination(Duration.ofSeconds(20).toMillis());
+
+      List<RunEvent> events =
+          handler.eventsContainer.stream()
+              .map(OpenLineageClientUtils::runEventFromJson)
+              .collect(Collectors.toList());
+
+      assertEquals(7, events.size());
+
+      List<RunEvent> kafkaInputEvents =
+          events.stream().filter(x -> !x.getInputs().isEmpty()).collect(Collectors.toList());
+
+      assertEquals(2, kafkaInputEvents.size());
+
+      kafkaInputEvents.forEach(
+          event -> {
+            assertEquals(1, event.getInputs().size());
+            assertEquals(kafkaContainer.sourceTopic, event.getInputs().get(0).getName());
+            assertEquals("kafka://" + bootstrapServers, event.getInputs().get(0).getNamespace());
+          });
+
+      List<RunEvent> outputEvents =
+          events.stream().filter(x -> !x.getOutputs().isEmpty()).collect(Collectors.toList());
+
+      assertEquals(4, outputEvents.size());
+
+      outputEvents.forEach(
+          event -> {
+            assertEquals(1, event.getOutputs().size());
+            assertEquals("openlineage.public.test", event.getOutputs().get(0).getName());
+            assertEquals(
+                postgresContainer.getNamespace(), event.getOutputs().get(0).getNamespace());
+          });
+
+      postgresContainer.stop();
+      kafkaContainer.stop();
+      spark.stop();
+    }
+
+    private HttpServer createHttpServer(HttpHandler handler) throws IOException {
+
+      int httpServerPort = new Random().nextInt(1000) + 10000;
+
+      HttpServer server = HttpServer.create(new InetSocketAddress(httpServerPort), 0);
+      server.createContext("/api/v1/lineage", handler);
+      server.setExecutor(null);
+      server.start();
+
+      return server;
+    }
+
+    private Dataset<Row> processKafkaTopic(Dataset<Row> input) {
+      StructType schema = StructType.fromDDL("id STRING, epoch LONG");
+      return input
+          .selectExpr("CAST(value AS STRING) AS value")
+          .select(from_json(col("value"), schema).as("event"))
+          .select(col("event.id").as("id"), col("event.epoch").as("epoch"))
+          .select(col("id"), from_unixtime(col("epoch")).as("timestamp"))
+          .select(functions.struct(col("id"), col("timestamp")).as("converted_event"))
+          .select(
+              expr("CAST('1' AS BINARY) AS key"),
+              functions.to_json(col("converted_event")).as("value"));
+    }
+
+    private Dataset<Row> readKafkaTopic(SparkSession spark, String topic, String bootstrapServers) {
+      return spark
+          .readStream()
+          .format("kafka")
+          .option("subscribe", topic)
+          .option("kafka.bootstrap.servers", bootstrapServers)
+          .option("startingOffsets", "earliest")
+          .load();
+    }
+
+    private PostgreSQLTestContainer startPostgresContainer() {
+      PostgreSQLContainer<?> postgres =
+          new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"))
+              .withDatabaseName("openlineage")
+              .withPassword("openlineage")
+              .withUsername("openlineage");
+
+      postgres.start();
+
+      return new PostgreSQLTestContainer(postgres);
+    }
+
+    private KafkaTestContainer setupKafkaContainer() {
+      KafkaContainer kafka =
+          new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1"));
+      kafka.start();
+
+      int kafkaTopicPrefix = new Random().nextInt(1000);
+      String kafkaSourceTopic = "source-topic-" + kafkaTopicPrefix;
+      String kafkaTargetTopic = "target-topic-" + kafkaTopicPrefix;
+
+      String bootstrapServers = kafka.getBootstrapServers();
+
+      createTopics(bootstrapServers, Arrays.asList(kafkaSourceTopic, kafkaTargetTopic));
+      populateTopic(bootstrapServers, kafkaSourceTopic);
+
+      return new KafkaTestContainer(kafka, kafkaSourceTopic, kafkaTargetTopic, bootstrapServers);
+    }
+
+    private SparkSession createSparkSession(Integer httpServerPort) {
+      String userDirProperty = System.getProperty("user.dir");
+      Path userDirPath = Paths.get(userDirProperty);
+      UUID testUuid = UUID.randomUUID();
+      log.info("TestUuid is {}", testUuid);
+
+      Path derbySystemHome =
+          userDirPath.resolve("tmp").resolve("derby").resolve(testUuid.toString());
+      Path sparkSqlWarehouse =
+          userDirPath.resolve("tmp").resolve("spark-sql-warehouse").resolve(testUuid.toString());
+
+      return SparkSession.builder()
+          .appName("kafka-source-kafka-sink")
+          .master("local[*]")
+          .config("spark.extraListeners", OpenLineageSparkListener.class.getCanonicalName())
+          .config("spark.driver.host", "localhost")
+          .config("spark.driver.extraJavaOptions", "-Dderby.system.home=" + derbySystemHome)
+          .config("spark.sql.warehouse.dir", sparkSqlWarehouse.toString())
+          .config("spark.ui.enabled", false)
+          .config("spark.openlineage.transport.type", "http")
+          .config("spark.openlineage.transport.url", "http://localhost:" + httpServerPort)
+          .config("spark.openlineage.facets.disabled", "[spark_unknown;]")
+          .getOrCreate();
     }
 
     private List<SchemaRecord> mapToSchemaRecord(OpenLineage.SchemaDatasetFacet schema) {
@@ -316,6 +559,7 @@ class SparkStreamingTest {
   }
 }
 
+@Tag("integration-test")
 class OpenLineageEndpointHandler implements HttpHandler {
   List<String> eventsContainer = new ArrayList<>();
 
